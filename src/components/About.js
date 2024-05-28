ad1d9435b63a3cb25fe4c03b6911f4926f349786
isha-ingersol <isha.ingersol@gmail.com>
2024-05-28 17:14:45 +0400

    About Page Final

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import { Container, Row, Col } from "react-bootstrap";
-import 'react-multi-carousel/lib/styles.css';
+import "react-multi-carousel/lib/styles.css";
 import EWCIsha from "../assets/img/ewc-semifinal1-02.jpg"
-import resume from '../assets/files/IshaIngersolResume.pdf';
+import resume from "../assets/files/IshaIngersolResume.pdf";
+
+import mail from "../assets/contact-images/email-icon.png"
+import github from "../assets/contact-images/github-icon.png"
+import linkedin from "../assets/contact-images/linkedin-icon.png"
 
 import '../css-components/About.css'
 
 export const About = () => {
+    // Function to copy the phone number
+    const copyPhoneNumber = async () => {
+        const phoneNumber = "+971504054363";
+
+        try {
+        await navigator.clipboard.writeText(phoneNumber);
+        alert("Phone number copied to clipboard: " + phoneNumber);
+        } catch (error) {
+        console.error("Error copying phone number: " + error.message);
+        }
+    };
+
   return (
     <section className="about" id="about"> 
         <Container>
@@ -48,12 +64,30 @@ export const About = () => {
                 </Col>
             </Row>
         </Container>
-
+                
         <div className="resume-button-container">
             <button className="resume-button" onClick={() => window.open(resume, '_blank')}>
                 FIND MY RESUME HERE!
             </button>
         </div>
+
+        <br />
+
+        <div className="social-icon" style={{display: "flex", justifyContent: "center", alignItems: "center", width: "30%", margin: "auto"}}>
+            <div className="email" style={{display: "flex", flexDirection: "column", alignItems: "center", paddingRight: "10%", paddingLeft: "10%"}}>
+                <a href="mailto:ingersol.isha@gmail.com" target="_blank" rel="noopener noreferrer"><img src={mail} alt="Mail" /></a>
+                <h4>Email</h4>
+            </div>
+            <div className="github" style={{display: "flex", flexDirection: "column", alignItems: "center", paddingRight: "10%"}}>
+                <a href="https://github.com/isha-ingersol" target="_blank" rel="noopener noreferrer"><img src={github} alt="GitHub" /></a>
+                <h4>GitHub</h4>
+            </div>
+            <div className="linkedin" style={{display: "flex", flexDirection: "column", alignItems: "center", paddingRight: "10%"}}>
+                <a href="https://www.linkedin.com/in/isha-ingersol/" target="_blank" rel="noopener noreferrer"><img src={linkedin} alt="LinkedIn" /></a>
+                <h4>LinkedIn</h4>
+            </div>
+        </div>
+
     </section>
   )
 }
