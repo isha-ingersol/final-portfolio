730e08be07b4b2cab3e8b76bfa2c21681dcd0287
isha-ingersol <isha.ingersol@gmail.com>
2024-10-05 13:31:42 +0100

    Updated Portfolio

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,6 @@
 import { useState, useEffect } from "react";
 import { Container, Row, Col, Nav } from "react-bootstrap";
-// import headerImg from "../assets/img/header-img.svg";
 import headerImg from "../assets/img/homescreen.png";
-import { ArrowRightCircle } from 'react-bootstrap-icons';
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
 import '../css-components/Home.css'
